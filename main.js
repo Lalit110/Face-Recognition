dc9d38ad5a57aef3c4633817e3b5ac73e747aaf2
Lalit110 <83270963+Lalit110@users.noreply.github.com>
2022-02-04 16:21:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,8 +17,25 @@ function take_snapshot(){
 
 console.log("ml5 version is -",ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/h2IriZpL1/model.json',model_loaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/lG-7lNjH0/model.json',model_loaded);
 
 function model_loaded(){
     console.log("model is loaded");
+}
+
+function check(){
+
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error,results){
+
+    if (error){
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = (results[0].confidence*100).toFixed(3)+"%";
+    }
 }
